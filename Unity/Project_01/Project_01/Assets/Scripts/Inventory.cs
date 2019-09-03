33323a85905ed1456a60c61777b594db2df30efc
Christopher Shipley <chris.shipleyland@gmail.com>
2019-09-03 15:28:07 -0600

    Started unit/item/inventory scripts.

---
+++ Unity/Project_01/Project_01/Assets/Scripts/Inventory.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    private List<Item> inventory;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public string ListInventory()
+    {
+        string list = "";
+        for (int i = 0; i < inventory.ToArray().Length; ++i)
+        {
+            list += inventory[i].name + "\n";
+        }
+        return list;
+    }
+
+
+    public void AddToInventory(Item newItem)
+    {
+        inventory.Add(newItem);
+
+    }
+
+
+    public Item GetItem(string itemName)
+    {
+        Item receiver = null;
+        for (int i = 0; i < inventory.ToArray().Length; ++i)
+        {
+            if (inventory[i].name == itemName)
+            {
+                receiver = inventory[i];
+                break;
+            }
+        }
+
+        return receiver;
+    }
+
+
+    public void RemoveFromInventory(string itemName)
+    {
+        Item remover = GetItem(itemName);
+        if (remover == null)
+        {
+            Debug.Log("There is not an item: \"" + itemName + "\" in this inventory.");
+        }
+        else
+        {
+            RemoveItem(remover);
+        }
+    }
+
+
+    private void RemoveItem(Item removedItem)
+    {
+        inventory.Remove(removedItem);
+    }
+}
---
+++ Unity/Project_01/Project_01/Assets/Scripts/Unit.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public double healthPoints; // The health point value of the character
+    public string name; // Name of the character
+    public Inventory inventory; // A list of generic Items
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void AddItem(Item newItem)
+    {
+        inventory.AddToInventory(newItem);
+    }
+
+    void RemoveItem(string itemName)
+    {
+        inventory.RemoveFromInventory(itemName);
+    }
+}
