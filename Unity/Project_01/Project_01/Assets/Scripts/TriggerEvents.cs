ac98c6c6fcc881fbd03852fc69c77f7665c1ed3e
Christopher Shipley <chris.shipleyland@gmail.com>
2019-09-10 15:43:07 -0600

    Introduction to power ups

---
+++ Unity/Project_01/Project_01/Assets/Scripts/TriggerEvents.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEvents : MonoBehaviour
+{
+    public UnityEvent triggerEvent;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        triggerEvent.Invoke();
+
+
+    } 
+}
