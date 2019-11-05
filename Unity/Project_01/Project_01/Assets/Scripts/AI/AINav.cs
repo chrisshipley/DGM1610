6564c0768c14af4b6aae19262947b2c51e04bde7
Christopher Shipley <chris.shipleyland@gmail.com>
2019-11-05 15:45:38 -0700

    AI Mesh Tutorial

---
+++ Unity/Project_01/Project_01/Assets/Scripts/AI/AINav.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+
+public class AINav : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public Vector3Data destination;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        destination.value = new Vector3(0, 0, 0);
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = destination.value;
+    }
+}
---
+++ Unity/Project_01/Project_01/Assets/Scripts/MouseEvents.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseEvents : MonoBehaviour
+{
+    private Camera cam;
+    public Vector3Data destination;
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log(cam.ScreenToWorldPoint(Input.mousePosition));
+        destination.value = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+
+
+
+}
