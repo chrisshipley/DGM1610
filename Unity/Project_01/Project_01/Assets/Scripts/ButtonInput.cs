cd478ea677dec97c298ff8b8f52b4723d7640057
Christopher Shipley <chris.shipleyland@gmail.com>
2019-11-19 15:51:22 -0700

    Animation Tutorial

---
+++ Unity/Project_01/Project_01/Assets/Scripts/ButtonInput.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ButtonInput : MonoBehaviour
+{
+    public UnityEvent inputEvent;
+    public string inputWord;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown(inputWord))
+        {
+            inputEvent.Invoke();
+        }
+    }
+}
